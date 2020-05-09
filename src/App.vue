2e09fbb872cf5c6f966714eaf6ac6810e6519db7
Lukas Lütke <lukasluetke3@gmail.com>
2020-05-09 21:01:57 +0200

    card

--- src/App.vue
+++ src/App.vue
@@ -40,7 +40,30 @@
             height="100%"
             flat
           >
-          <span>bla</span>
+          <!-- card: -->
+          <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      src="./containern3.png"
+    >
+      <v-card-title>EDEKA Summerer</v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pb-0">ab 20 Uhr</v-card-subtitle>
+
+    <v-card-text class="text--primary">
+      <div>Links am Gebäude vorbeigehen. Dort befindet sich ein großer grüner Container!</div>
+    </v-card-text>
+
+    <v-card-actions>
+  <v-btn block color="green" dark>Status: Heute noch nicht geleert!</v-btn>
+    </v-card-actions>
+  </v-card>
+  <!-- card end -->
           </v-card>
         </v-tab-item>
       </v-tabs-items>
